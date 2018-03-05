7931cb0795a63b5960e93691bcfc6bfc81cc7909
MirroredFate <Karl_daniel@hotmail.de>
2018-03-05 11:16:26 +0100

    Player Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z-1);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       RaycastHit hit;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                navMeshAgent.destination = hit.point;
+                Debug.Log("I'm moving.");
+            }
+            else
+            {
+                Debug.Log("Nothing found");
+            }
+        }
+    }
+}
