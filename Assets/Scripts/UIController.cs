889c79a7315005cc246740713f4f0b750ac581d8
MirroredFate <Karl_daniel@hotmail.de>
2018-03-07 15:20:47 +0100

    fixed some structure issues

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,36 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour
-{
+public class UIController : MonoBehaviour {
+
+
     public Text text;
-
+    float minutes = 0;
+    float seconds = 0;
     float timerCount;
-    float timerStart; 
+    float timerStart;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
 
-    }
+        //Funktioniert nicht
+         /*
+         seconds = Mathf.Floor(Time.time % 60);
+         minutes = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
+         if (seconds > 59) minutes += 1;
 
-        #region GameTimer
+
+         //Text Ausgabe
+         if(seconds <= 9 && minutes == 0) text.text = "00:0" + seconds.ToString();
+         if(seconds >= 10 && minutes == 0) text.text = "00:" + seconds.ToString();
+         if(minutes <= 9 && seconds <= 9) text.text = "0" + minutes.ToString() + ":0" + seconds.ToString();
+         if(minutes >=10 && seconds <= 9) text.text = minutes.ToString() + ":0" + seconds.ToString();
+         if(minutes >=10 && seconds >= 10) text.text = minutes.ToString() + ":" + seconds.ToString();
+         */
+
+        //Internet Variante
 
         timerCount = Time.time - timerStart;
         timerStart = Time.time - timerCount;
 
-        float seconds = Mathf.Floor(timerCount % 60);
-        float minutes = Mathf.Floor(timerCount / 60);
+        seconds = Mathf.Floor(timerCount % 60);
+        minutes = Mathf.Floor(timerCount / 60);
 
-
-        //Text Ausgabe
+        //Ausgabe
         text.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
-        #endregion
+
+
 
     }
 }
